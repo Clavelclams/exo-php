0211c2b1d9ee6306dd94364e8bca7ce55513bf8a
Claveliito <clavelclams12@gmail.com>
2024-11-13 09:22:35 +0100

    test 1

--- affiche_nombre_impaire.php
+++ affiche_nombre_impaire.php
@@ -1,8 +1,22 @@
 <?php
-    
-
-
-
-
+/*
+for ($a = 0; $a <= 150; $a++) {
+    if ($a % 2 != 0){
+        echo $a . " ";
+    }
+};
+*/
+/* 
+$a = "Je dois faire des sauvegardes régulières de mes fichiers ";
+for ($b = 0; $b < 500; $b++) {
+    echo $a . "<br>";
+}
+*/
+echo "<table border='1' cellpanding='5' cellspancing='0' style='border-collapse: collapse;'>";
+echo "<tr><th>*</th>";
+for ($a = 1; $a <= 9; $a++) {
+    echo "<th>$a</th>";
+}
+echo "</tr>";
 
 ?>
