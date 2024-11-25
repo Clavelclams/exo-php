7c7783adab8cae379fce36e8a61a7437833009e6
Claveliito <clavelclams12@gmail.com>
2024-11-25 16:01:51 +0100

    tt

--- function.php
+++ function.php
@@ -12,10 +12,42 @@ function salutation ($texte, $nom) {
 
 salutation($texte,"Machin");
 */
+/*
 $a = $b = 2;
 function somme() {
+    global $a, $b;
     $c = $a + $b;
-    echo "$c";    
+    echo "$c euro";    
 }
 somme()
+*/
+function test() {
+    $a=0;
+    echo $a."<br>";
+    $a++;
+}
+test();
+test();
+
+function test1() {
+    static $a=0;
+    echo $a."<br>";
+    $a++;
+}
+test1();
+test1();
+test1();
+
+// Training :
+
+
+function lien($url, $titre){
+    $lien = "<a href=\"$url\">$titre</a>";
+    
+    return $url;
+}
+
+echo lien("https://www.reddit.com/", "Reddit Hug");
+
+
 ?>
