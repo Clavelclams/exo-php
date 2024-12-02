77a31dd969e6d2e3f29e9cb4c5f37ce8413b5615
Claveliito <clavelclams12@gmail.com>
2024-12-02 12:45:55 +0100

    mon fichier se telecharge pas je vois ça pares la pause

--- check.php
+++ check.php
@@ -8,8 +8,18 @@ if (isset($_FILES['fichier'])) {
         $uploadDir = 'uploads/';
         $uploadFile = $uploadDir . basename($_FILES['fichier']['name']);
         if (!is_dir($uploadDir)) {
-            mkdir($)
+            mkdir($uploadDir, 0777, true);
         }
+        if (move_uploaded_file($_FILES['fichier']['tmp_name'], $uploadFile)) {
+            echo "le fichier est téléchargé, merci !";
+        } else {
+            echo "Echec du téléchargement";
+        }
+    } else {
+        echo "Erreur de téléchargement : " . $_FILES['fichier']['error'];
     }
+} else {
+    echo "Aucun fichier téléchargé.";
+
 }
 ?>
